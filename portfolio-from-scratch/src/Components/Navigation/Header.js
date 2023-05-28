329060b3e24e5f6a45f6f3259d3d11e5d049ec3e
Harman <harmany@hotmail.ca>
2023-05-28 16:39:41 -0400

    minor cleaning

--- portfolio-from-scratch/src/Components/Navigation/Header.js
+++ portfolio-from-scratch/src/Components/Navigation/Header.js
@@ -9,10 +9,10 @@ const Header = () => {
       <div className="container d-flex justify-content-between align-items-center">
         <div className="header-title text-white">My Dev Portfolio</div>
         <div className="socials">
-          <a href="https://twitter.com/your_username">
+          <a href="https://www.linkedin.com/in/harman-s-mann/">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
-          <a href="https://instagram.com/your_username">
+          <a href="https://github.com/HarmanSMann">
             <FontAwesomeIcon icon={faGithub} />
           </a>
         </div>
