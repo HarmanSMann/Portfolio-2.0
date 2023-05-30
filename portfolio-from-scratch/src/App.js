5d47be1a7a3cf1ecab460966b2030b7fd7755be0
Harman <harmany@hotmail.ca>
2023-05-29 21:00:55 -0400

    basic footer acomplished

--- portfolio-from-scratch/src/App.js
+++ portfolio-from-scratch/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Components/Navigation/Header'
+import Footer from './Components/Navigation/Footer'
 
 const App = () => {
   return (
     <>
       <Header/>
+      <Footer/>
     {/* 
     Welcome
     About me -> link buttons to: linkedin, github
---
+++ portfolio-from-scratch/src/Components/Navigation/Footer.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faUser,
+  faBriefcase,
+  faEnvelope,
+  faPlayCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <nav className="footer-navigation">
+          <ul className="footer-nav-list">
+            <li className="footer-nav-item">
+              <button className="footer-button">
+                <FontAwesomeIcon icon={faHome} />
+                <span className="footer-button-text">Home</span>
+              </button>
+            </li>
+            <li className="footer-nav-item">
+              <button className="footer-button">
+                <FontAwesomeIcon icon={faPlayCircle} />
+                <span className="footer-button-text">Video</span>
+              </button>
+            </li>
+            <li className="footer-nav-item">
+              <button className="footer-button">
+                <FontAwesomeIcon icon={faUser} />
+                <span className="footer-button-text">About</span>
+              </button>
+            </li>
+            <li className="footer-nav-item">
+              <button className="footer-button">
+                <FontAwesomeIcon icon={faBriefcase} />
+                <span className="footer-button-text">Portfolio</span>
+              </button>
+            </li>
+            <li className="footer-nav-item">
+              <button className="footer-button">
+                <FontAwesomeIcon icon={faEnvelope} />
+                <span className="footer-button-text">Contact</span>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
